9be2d5529caab92e956915738f5feaa9c0e4314c
pranavjaradi <jaradi7p@gmail.com>
2023-02-05 14:01:13 +0530

    Starting the journey

---
+++ letterCounter.py
@@ -0,0 +1,7 @@
+#Basic Letter Counter App
+
+print("Welcome to the Letter counter App")
+
+#Get user name
+nameOfUser = input("\nWhat is your name: ").title().strip()
+print("Hello and welcome " + nameOfUser + "!")
