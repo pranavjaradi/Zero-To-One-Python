f7544aad5edfb2eb600c0f8ef81d0d4fb952de57
pranavjaradi <jaradi7p@gmail.com>
2023-02-05 14:22:41 +0530

    Final commit for letter counter app

--- letterCounter.py
+++ letterCounter.py
@@ -8,4 +8,17 @@ print("Hello and welcome " + nameOfUser + "!")
 
 print("I will count the frequency of specific letter in a message.")
 
+#Taking message from user and converting to lower case for standardising
+messageByUser = input("\nPlease enter your message: ").lower()
 
+#Taking user input for counting the frquency of letter
+letterByUser = input("Which letter would you like to count the frequency of: ").lower()
+
+
+#Counting the letter frequency in message using count() and displaying result
+if (len(letterByUser) == 1):
+    letterFrequency = messageByUser.count(letterByUser)
+    print("\n" + nameOfUser + ", your message has " + str(letterFrequency) + " " + letterByUser + "'s in it.")
+    print ("Thank You!!")
+else:
+    print("Please enter valid letter as input.")
