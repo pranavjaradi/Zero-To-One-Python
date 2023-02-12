ab8a3ad68b6dc8ced695b659dad12e9ad8e89277
pranavjaradi <jaradi7p@gmail.com>
2023-02-12 16:16:54 +0530

    Adding perimeter in triangle solver app.

--- rightTriangleSolverApp.py
+++ rightTriangleSolverApp.py
@@ -13,7 +13,9 @@ side_c = math.sqrt( (side_a ** 2) + (side_b ** 2) )
 side_c = round(side_c, 3)
 areaOfTriangle = 0.5 * side_a * side_b
 areaOfTriangle = round(areaOfTriangle, 3)
+perimeterOfTriangle = round((side_a+side_b+side_c),3)
 
 #Printing results with different formatting
 print("\nHypotenuse of a triangle with sides {} and {} is {}.".format(side_a,side_b,side_c))
 print(f"\nArea of the triangle with sides {side_a} and {side_b} is {areaOfTriangle}.")
+print("\nPerimeter of triangle with sides as {}, {} and {} is {}.".format(side_a,side_b,side_c,perimeterOfTriangle))
