efeaf4d9c2c6028a3a8b764ba6102825ad853051
pranavjaradi <jaradi7p@gmail.com>
2023-03-08 18:51:53 +0530

    Create 16 Shipping Accounts Program.py

---
+++ 16 Shipping Accounts Program.py
@@ -0,0 +1,12 @@
+"""
+Shipping Accounts Program
+You are responsible for writing a program that will simulate logging into a business’s shipping
+accounts software. Once logged in your program will display the current costs of shipping x
+amount of items. Based on the number of items shipped, the cost to ship each item will vary.
+Once the cost to ship an item is set, your program will calculate the cost of shipping the entire
+order. Upon confirmation of the order, your program will place the order and prepare the
+shipment.
+"""
+
+print("Welcome to the Shipping Accounts Program.")
+
