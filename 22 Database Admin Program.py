2faa4449c85642c9f3633094d1932c3baefd7e14
pranavjaradi <jaradi7p@gmail.com>
2023-03-18 22:24:11 +0530

    Initial Database Admin Program with problem statement.

---
+++ 22 Database Admin Program.py
@@ -0,0 +1,11 @@
+"""
+Database Admin Program:
+You are responsible for writing a program that will simulate logging into a database and
+prompting a user to change their password. All usernames and passwords to the database will
+be stored in a dictionary. Upon entering the correct credentials, your program will prompt the
+user to enter a new password that is a minimum of eight characters long. If the new password
+meets the criteria, it will be accepted, otherwise the new password will be rejected. If the user
+who logged in is the admin, a list of all usernames and passwords will be displayed.
+"""
+
+print("Welcome to Database Admin Program.")
