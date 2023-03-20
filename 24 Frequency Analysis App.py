8d125f7d4e59a304a0b494428c995a4a6ef4496b
pranavjaradi <jaradi7p@gmail.com>
2023-03-20 12:02:36 +0530

    Frequency Analysis app

--- 24 Frequency Analysis App.py
+++ 24 Frequency Analysis App.py
@@ -6,12 +6,14 @@ letter within the text, calculate the percentage of occurrence for each letter, 
 letters ordered from highest occurrence to lowest occurrence. Your program will perform these
 operations for two different bodies of text.
 """
+
 from collections import Counter
 
 print("Welcome to the Frequency Analysis app.")
 
 #list of letters to keep in string
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 
 #taking strinng from user
 key_phrase_1 = input("Enter a word or phrase to count the occurrence of each letter: ").lower().strip()
@@ -31,4 +33,30 @@ for key, value in sorted(letter_count.items()):
     print(key + "\t" + str(value) + "\t\t" + str(round(value*100/total_letters, 2)) + "%")
 
 #printing letters in highest to lowest order
+for element in letter_count.most_common():
+    print(element[0],end='')
+    
 
+#taking strinng from user for second string
+key_phrase_1 = input("\nEnter a word or phrase to count the occurrence of each letter: ").lower().strip()
+
+#removing all non-alphabets
+for letter in key_phrase_1:
+    if letter not in letters:
+        key_phrase_1 = key_phrase_1.replace(letter, '')
+
+#Counting occurence of each letter and total letters
+letter_count = Counter(key_phrase_1)
+total_letters = len(key_phrase_1)
+
+#Printing frequency of each letter
+print("Letter\tOccurence\tPercentage")
+for key, value in sorted(letter_count.items()):
+    print(key + "\t" + str(value) + "\t\t" + str(round(value*100/total_letters, 2)) + "%")
+
+#printing letters in highest to lowest order
+for element in letter_count.most_common():
+    print(element[0],end='')
+    
+#Hello! How are you doing today? 32 years old I'm today.
+#: This is prety interesting. I feel like i'm learning something new!
