8cb81b3f66c90e489b765b07c4b8ae30ec76862d
pranavjaradi <jaradi7p@gmail.com>
2023-03-22 20:39:13 +0530

    Prime Number app

--- 6 While loops challenges/28 Prime Number App.py
+++ 6 While loops challenges/28 Prime Number App.py
@@ -7,6 +7,8 @@ took to the user. It is important to time certain processes within our programs 
 efficient our code is.
 """
 
+import time
+
 print("welcome to the Prime Number App.")
 
 #Flag for keeping loop running
@@ -24,9 +26,53 @@ while running:
         prime_flag = True
         for i in range(2, number):
             if number%i == 0:
+                #If we get 0 reminder when our number is divided by 2 to number then breaking the loop and setting prime_flag = false.
                 prime_flag = False
                 break
-        if prime_flag == False:
-            print("Number is not prime")
+        if prime_flag:
+            print("{} is prime!".format(number))
         else:
-            print("Number is prime")
+            print("{} is not prime!".format(number))            
+
+    elif choice == '2':
+        #taking upper and lower bound for finding prime numbers between them
+        lower_bound = int(input("\nEnter the lower bound of your range: "))
+        upper_bound = int(input("Enter the upper bound of your range: "))
+        
+        #Creating a list which will contain all prime numbers and taking start time to calculate time taken for calculation.
+        primes = []
+        start_time = time.time()
+        
+        #Running loop for finding prime
+        for prime_candidate in range(lower_bound, upper_bound):
+            prime_status = True
+            if prime_candidate == 1:
+                prime_status = False
+            else:
+                for i in range(2, prime_candidate):
+                    if prime_candidate % i == 0:
+                    #If we get 0 reminder when our number is divided by 2 to number then breaking the loop and setting prime_flag = false.
+                        prime_status = False
+                        break
+                if prime_status:
+                    primes.append(prime_candidate)
+        end_time = time.time()
+        time_delta = round(end_time-start_time,4)
+
+
+        #Printing Summary
+        print("\nCalculation took a total of " + str(time_delta) + " seconds.")
+        print("The following numbers between {} and {} are prime.".format(lower_bound, upper_bound))
+        input("Press enter to continue.")
+        for prime in primes:
+            print(prime)
+
+    else:
+        #If user invalid input ie not 1 or 2.
+        print("That is not a valid option.")
+
+    #Asking user to continue program or not
+    choice = input("Would you like to run the program again (y/n): ")
+    if choice != 'y':
+        running = False
+        print("\nThank you for using the program. Have a nice day.")
