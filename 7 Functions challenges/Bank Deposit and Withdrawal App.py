ac9763ce47e238b054b7d5a522f16aba1bc02291
pranavjaradi <jaradi7p@gmail.com>
2023-03-26 14:08:12 +0530

    optimising

---
+++ 7 Functions challenges/Bank Deposit and Withdrawal App.py
@@ -0,0 +1,8 @@
+"""
+Bank Deposit and Withdrawal App:
+You are responsible for writing a program that will simulate an online banking application. A
+user will create an account with your fictitious bank. The account will include a savings account
+and a checking account. Users will then be able to make deposits or withdrawals from either
+account as long as the remaining balance is non negative.
+"""
+
