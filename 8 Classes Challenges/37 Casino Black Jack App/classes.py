abd402eb9bf174aa4a2a9824c7d000ffbb921838
pranavjaradi <jaradi7p@gmail.com>
2023-04-17 01:03:41 +0530

    Initial commit Black Jack app

---
+++ 8 Classes Challenges/37 Casino Black Jack App/classes.py
@@ -0,0 +1,137 @@
+import random
+
+
+class Card():
+    """
+    Card class which will create a card object and a method to print card info
+    """
+    def __init__(self, rank, value, suit):
+        """
+        Card object creation, taking rank, value and suit of card
+
+        Args:
+            rank (string): It will be rank of card which will have values (2-10, J ,Q ,K ,A)
+            value (integer): Value of each will the corresponding value of the card.
+            suit (string): It is suit of the card viz. hearts, spades, clubs, and diamonds
+        """
+        self.rank = rank
+        self.value = value
+        self.suit = suit
+
+    def display_card(self):
+        """
+        Printing the card information eg: K of Hearts, 10 of Spades etc.
+        """
+        print("{} of {}".format(self.rank, self.suit))
+
+class Deck():
+    """
+    Deck class for building, shuffling deck and dealing card
+    """
+    def __init__(self):
+        """
+        Contains list of deck of cards
+        """
+        self.cards = []
+
+    def build_deck(self):
+        """
+        Build a deck containing 13 cards of each of 4 deck equivalent to total 52 cards.
+        """
+        suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds']
+        ranks = {'A' : 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8' : 8, '9': 9, 'J': 10, 'Q': 10, 'K': 10}
+        
+        #Looping through each suit to build deck using dictionary
+        for suit in suits:
+            for rank, value in ranks.items():
+                card = Card(rank, value, suit)
+                self.cards.append(card)
+
+    def shuffle_deck(self):
+        """Generating a shuffled card using random module
+        """
+        random.shuffle(self.cards)
+    
+    def deal_card(self):
+        """Dealing a card from deck of cards
+
+        Returns:
+            object: Instance of class Card.
+        """
+        return self.cards.pop()
+
+
+
+class Player():
+    """A class for user playing Black Jack
+    """
+    def __init__(self):
+        """Initialise the player
+        """
+        self.hand = [] #List for player's card
+        self.hand_value = 0 #Total value of player hand
+        self.playing_hand = True #Bool to track if player is playing hand or not
+    
+    def draw_hand(self, deck):
+        """Deal the player the starting hand
+
+        Args:
+            deck (list): Deck of card
+        """
+        for i in range(2):
+            #Two cards are must for game to start
+            card = deck.deal_card()
+            self.hand.append(card)
+    
+    def display_hand(self):
+        """Show the players hand"""
+        print("\nPlayer's hand: ")
+        for card in self.hand:
+            card.display_card()
+    
+    def hit(self, deck):
+        """Give a new card to player"""
+        card = deck.deal_card()
+        self.hand.append(card)
+    
+    def get_hand_value(self):
+        """Compute the hand value"""
+        self.hand_value = 0
+        ace_in_hand = False #bool to track if player have an ace
+        for card in self.hand:
+            hand_value += card.value
+            if card == 'A':
+                #checked for ace
+                ace_in_hand = True
+        
+        if self.hand_value > 21 and ace_in_hand == True:
+            #Treating ace value 1 if hand value is more than 21 and player has an ace
+            self.hand_value -= 10 #Ace value is 11 but treating here as 1 so deducting 1-
+        
+        print("Total value: {}".format(self.hand_value))
+    
+    def update_hand(self, deck):
+        """Updating hannd of player by allowing them to hit
+
+        Args:
+            deck (list): Deck of cards
+        """
+        #The player has option to hit
+        if self.hand_value < 21:
+            choice = input("Would you like to hit(y/n): ").lower()
+            if choice.startswith('y'):
+                self.hit(deck)
+            else:
+                self.playing_hand = False
+        #player is over 21, can not hit again
+        else:
+            self.playing_hand = False
+
+
+# class Dealer():
+#     def __init__(self):
+
+
+
+# class Game():
+#     def __init__(self):
---
+++ 8 Classes Challenges/37 Casino Black Jack App/main.py
@@ -0,0 +1,11 @@
+"""
+Casino Black Jack App:
+You are responsible for writing a program that allows a user to play casino Black Jack. The
+user will put a set amount of money onto the table and make a minimum $20 bet each hand.
+Each hand, the user will be dealt two cards and be given the option to hit or stay. If the user hits
+21 or goes over the round will end. The dealer will continue to hit until their hand has a
+minimum value of 17 as per casino guidelines. The user will be able to play as long as their
+total money is greater than or equal to the minimum bet of the table.
+"""
+
+from gclasses import *
